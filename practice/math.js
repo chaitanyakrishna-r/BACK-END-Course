2d885255a3cd09642408c78bd90e5d398eedd324
Chaitanyakrishna-r <chaitanyakrishna1120@gmail.com>
2026-01-11 20:26:03 +0530

    practices mongoDB

--- practice/math.js
+++ practice/math.js
@@ -5,12 +5,12 @@ export const mul =(a, b) => a*b;
 export const g =9.87;
 export const PI = 3.14;
 
-// let obj ={
-//     sum:sum,
-//     mul:mul,
-//     g:g ,
-//     PI:PI
-// }
+let obj ={
+    sum:sum,
+    mul:mul,
+    g:g ,
+    PI:PI
+}
 module.exports ={
     sum:sum,
     mul:mul,
