64e325b5d267980009a8970cf66639deab629802
Chaitanyakrishna-r <chaitanyakrishna1120@gmail.com>
2026-01-24 21:18:40 +0530

    express dir

--- Back-End/ExpressDir/index.js
+++ Back-End/ExpressDir/index.js
@@ -7,11 +7,11 @@ app.listen(port, ()=>{
     console.log(`app is listening on ${port}`);
 })
 
-// app.use((req,res)=>{
-//     console.log("request received");
-//     let code = "<h1>Fruits</h1><ul><li>apple</li><li>orange</li></ul>"
-//     res.send(code);
-// }) 
+app.use((req,res)=>{
+    console.log("request received");
+    let code = "<h1>Fruits</h1><ul><li>apple</li><li>orange</li></ul>"
+    res.send(code);
+}) 
 
 
 let code = "<h1>Fruits</h1><ul><li>apple</li><li>orange</li></ul>"
