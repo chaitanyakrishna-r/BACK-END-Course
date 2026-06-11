594e7850fdd6f8e3e8f418c06d5d48d0e238d9ee
Chaitanyakrishna-r <chaitanyakrishna1120@gmail.com>
2026-06-11 21:49:13 +0530

    cloudinary confing

--- Back-End-Yt/src/routes/user.route.js
+++ Back-End-Yt/src/routes/user.route.js
@@ -7,6 +7,7 @@ import {
   changeCurrentPassword,
   getChannelProfile,
   createSub,
+  getWatchHistory
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -34,18 +35,11 @@ router.post("/refresh-token", regenerateAccessToken);
 
 router.route("/changeCurrentPassword").post(verifyJWT, changeCurrentPassword);
 
-router.route("/updateUserAvatar").post(upload.fields([
-  {
-    name:"avatar",
-    maxCount:1,
-  },{
-    name: "coverImage",
-    maxCount:1,
-  }
-]))
+router.route("/updateUserAvatar").patch(upload.single("avatar"))
 
 router.route('/subscribe').post(createSub)
 
-router.route('/getChannelProfile/:username').get(getChannelProfile);
+router.route('/getChannelProfile/:username').get(verifyJWT,getChannelProfile);
+router.route('/watchHistory').get(verifyJWT, getWatchHistory);
 
 export default router;
